0ff5c24bcbfc98218fd242bdeea92757a3c36022
Soham-Patel <sohampatel12@gmail.com>
2017-10-30 21:24:44 +0530

    reset

--- views/js/main.js
+++ views/js/main.js
@@ -500,20 +500,14 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-  var topPosition = document.body.scrollTop / 1250;
-  var phases = [];
-  for (var i = 0; i < 5; i++) {
-    phases.push(Math.sin(topPosition + i));
-  }
 
-  // items = document.getElementsByClassName('mover');
+  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    // var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    /* moved the Math.sin calc above out of the loop so it just gets called
-     * once, when the function is called */
-    items[i].style.left = items[i].basicLeft + 100 * phases[i % 5] + 'px';
+    // document.body.scrollTop is no longer supported in Chrome.
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
-
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
@@ -532,8 +526,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  var movingPizzas = document.getElementById('movingPizzas1');
-  for (var i = 0; i < 35; i++) {
+  for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
@@ -541,9 +534,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    movingPizzas.appendChild(elem);
+    document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
-    var items = document.querySelectorAll('.mover');
-
 });
