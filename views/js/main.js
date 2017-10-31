323846f043f794b7a3b2da95e163fa3ab9b31b6f
Soham-Patel <sohampatel12@gmail.com>
2017-10-31 15:44:59 +0530

    for loop optimisation

--- views/js/main.js
+++ views/js/main.js
@@ -503,11 +503,15 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
+
+
+  phases = [];
+  for (var j = 0; j < 5; j++){
+    phases.push(Math.sin((document.body.scrollTop / 1250) + (j % 5)));
+  }
+  
   for (var i = 0; i < items.length; i++) {
-    // document.body.scrollTop is no longer supported in Chrome.
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + 100 * phases[(i % 5)] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
